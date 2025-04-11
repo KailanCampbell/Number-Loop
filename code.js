bd3e806da623eaaf3d2bb32c915752a89e24e838
KailanCampbell <thehermitcrabmapper@gmail.com>
2025-04-11 12:28:22 -0400

    Create code.js

---
+++ code.js
@@ -0,0 +1,13 @@
+for (let i = 1; i <= 20; i++) {
+  const p = document.createElement("p");
+  p.textContent = i;
+
+  if (i % (i / 2) = 0) {
+    p.style.color = "blue";
+  } else {
+    p.style.color = "red";
+  }
+
+  container.appendChild(p);
+}
+
