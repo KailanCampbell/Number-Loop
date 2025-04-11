0d2285156795564774ddce556311af8b7fafb75f
KailanCampbell <thehermitcrabmapper@gmail.com>
2025-04-11 12:35:51 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,11 @@
+const container = document.getElementById("list-container");
+
+for (let i = 1; i <= 5; i++) {
+  const p = document.createElement("p");
+  p.textContent = "This is item #" + i;
+  container.appendChild(p);
+}
+
 for (let i = 1; i <= 20; i++) {
   const p = document.createElement("p");
   p.textContent = i;
