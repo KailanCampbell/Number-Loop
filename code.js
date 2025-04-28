b48888043fd6557d555ab4e621a14c4aee6336b6
KailanCampbell <thehermitcrabmapper@gmail.com>
2025-04-28 13:17:28 -0400

    Update code.js

--- code.js
+++ code.js
@@ -3,7 +3,7 @@ for (var i = 1; i <= 20; i++) {
   const p = document.createElement("p");
   p.textContent = "This is item #" + i;
 
- if (i % 2 = 0) {
+ if (i % 2 == 0) {
     p.style.color = "blue";
   } 
  else {
