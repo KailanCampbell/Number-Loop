40dc330d0a1f38b754088a55080f4b548de8b2f0
KailanCampbell <thehermitcrabmapper@gmail.com>
2025-04-28 13:15:07 -0400

    Update code.js

--- code.js
+++ code.js
@@ -3,12 +3,12 @@ for (var i = 1; i <= 20; i++) {
   const p = document.createElement("p");
   p.textContent = "This is item #" + i;
 
-  if (i % 2 = 0) {
+ if (i % 2 = 0) {
     p.style.color = "blue";
-  } else {
+  } 
+ else {
     p.style.color = "red";
   }
 
   container.appendChild(p);
 }
-
