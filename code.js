1634ab3c4534906c49ff6cd18958fab237ff4735
KailanCampbell <thehermitcrabmapper@gmail.com>
2025-04-28 13:13:11 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,9 +1,9 @@
 const container = document.getElementById("list-container");
 for (var i = 1; i <= 20; i++) {
   const p = document.createElement("p");
- p.textContent = "This is item #" + i;
+  p.textContent = "This is item #" + i;
 
-  if i % 2 = 0 {
+  if (i % 2 = 0) {
     p.style.color = "blue";
   } else {
     p.style.color = "red";
